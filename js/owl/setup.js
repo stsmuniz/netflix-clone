3c99e3ff891581b1211c75c404efcd42c8992e82
Salustiano Muniz <stsmuniz@gmail.com>
2021-10-03 22:41:27 -0300

    clone interface netflix, mobile

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  dots: false,
+  responsive: {
+    0: {
+      items: 2,
+      nav: false,
+    },
+    600: {
+      items: 3,
+      nav: true,
+    },
+    1000: {
+      items: 5,
+      nav: true,
+    },
+  },
+});
